1f4bb4b8e239e1b0d2b595b9dd40e6376c8bcaa4
Gerardo Rico <87782061+gerardoricor@users.noreply.github.com>
2022-04-27 18:05:35 -0600

    Add files via upload

---
+++ ERC20.sol
@@ -0,0 +1,98 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 < 0.7.0;
+pragma experimental ABIEncoderV2;
+import "./SafeMath.sol";
+
+//Interface of our ERC20 token
+interface IERC20{
+    //Returns the number of tokens in stock
+    function totalSupply() external view returns (uint256);
+
+    //Returns the number of tokens for an address specified by parameter
+    function balanceOf(address account) external view returns (uint256);
+
+    //Returns the number of tokens that the spender will be able to spend on behalf of the owner.
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    //Returns a boolean value resulting from the specified operation
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    //Returns a boolean value with the result of the expense operation
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    //Returns a boolean value with the result of the operation of passing a number of tokens using the allowance() method
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+
+    //Event to be emitted when a quantity of tokens passes from an origin to a destination.
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    //Event to be emitted when an assignment is established with the allowance() method.
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+//Implementation of ERC20 token functions
+contract ERC20Basic is IERC20{
+
+    string public constant name = "GECCOIN";
+    string public constant symbol = "GCO";
+    uint8 public constant decimals = 2;
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(address indexed owner, address indexed spender, uint256 tokens);
+
+
+    using SafeMath for uint256;
+
+    mapping (address => uint) balances;
+    mapping (address => mapping (address => uint)) allowed;
+    uint256 totalSupply_;
+
+    constructor (uint256 initialSupply) public{
+        totalSupply_ = initialSupply;
+        balances[msg.sender] = totalSupply_;
+    }
+
+
+    function totalSupply() public override view returns (uint256){
+        return totalSupply_;
+    }
+
+    function increaseTotalSupply(uint newTokensAmount) public {
+        totalSupply_ += newTokensAmount;
+        balances[msg.sender] += newTokensAmount;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint256){
+        return balances[tokenOwner];
+    }
+
+    function allowance(address owner, address delegate) public override view returns (uint256){
+        return allowed[owner][delegate];
+    }
+
+    function transfer(address recipient, uint256 numTokens) public override returns (bool){
+        require(numTokens <= balances[msg.sender]);
+        balances[msg.sender] = balances[msg.sender].sub(numTokens);
+        balances[recipient] = balances[recipient].add(numTokens);
+        emit Transfer(msg.sender, recipient, numTokens);
+        return true;
+    }
+
+    function approve(address delegate, uint256 numTokens) public override returns (bool){
+        allowed[msg.sender][delegate] = numTokens;
+        emit Approval(msg.sender, delegate, numTokens);
+        return true;
+    }
+
+    function transferFrom(address owner, address buyer, uint256 numTokens) public override returns (bool){
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+        emit Transfer(owner, buyer, numTokens);
+        return true;
+    }
+}
